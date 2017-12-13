90d5de167ebd60f806512fda9b93b801dee90601
arif98741 <arifsofg@gmail.com>
2017-12-13 23:52:28 +0600

    Add Realtime Search Folder

---
+++ 13-12-2017-realtime-search/index.php
@@ -0,0 +1,21 @@
+<?php include 'header.php'; ?>
+<div class="phpdarkcoding">
+    <section class="headeroption">
+        <h2>PHP Login System</h2>
+    </section>
+
+    <section class="mainoption">
+        <h1 class="success">Realtime Search</h1>
+        <h3 class="success">From Here Seach Your Query</h3>
+        <hr/>
+        <div class="contentoption">
+            <input type="text" placeholder="Enter Email or Phone" id="searchbar">
+           <br/>
+
+           <div class="message">
+              
+           </div>
+        </div>
+
+    </section>
+    <?php include 'footer.php'; ?>
