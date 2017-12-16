4b42ac991c0921e6e7a8bae3274da41d3095c026
arif98741 <arifsofg@gmail.com>
2017-12-16 19:40:47 +0600

    Add Comment and Break for php

---
+++ 02-12-2017/comment_and_break.php
@@ -0,0 +1,17 @@
+<?php 
+
+	$x = 5;
+	$y = 6;
+	//$z = 9; // is used for single line comment
+	/* for comment on multiple line*/ 
+
+
+
+	//for break you need to use br tag of html in echo
+	 echo "Hellow John";
+	 echo "<br/>";
+	 echo "Yes, I am here";
+
+
+
+?>
