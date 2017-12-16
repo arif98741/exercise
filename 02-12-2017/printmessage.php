145e69144fbc5701c4122ddc61f6d72230137341
arif98741 <arifsofg@gmail.com>
2017-12-16 19:38:00 +0600

    Add Printmessage.php for print

---
+++ 02-12-2017/printmessage.php
@@ -0,0 +1,14 @@
+<?php
+	//for printing php you need to declare first variables . keep values in variable and finallay print them. you need to use word echo for printing
+	$number1 = 5;
+	$number2 = 10.23;
+	$message = "Hellow World";
+
+	echo $number1; //print number1
+	echo "<br/>"; //print break
+	echo $number2; //print number2
+	echo "<br/>"; //print break
+	echo $message; //print message
+	
+
+ ?>
