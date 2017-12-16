9ed6a4cf304cd091113b1b5ccdfaff8083892801
arif98741 <arifsofg@gmail.com>
2017-12-16 19:46:35 +0600

    Add Concat.php file

---
+++ 02-12-2017/concat.php
@@ -0,0 +1,15 @@
+<?php 
+	//for concat in php it is need to use dot (.)
+
+	$name = "Ariful Islam";
+	echo "My name is ".$name;
+
+	echo "<br/>";
+
+	$h = "Hello";
+	$w = "World";
+	echo $h." ".$w;
+	
+
+
+?>
