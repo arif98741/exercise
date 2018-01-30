bcb54284ac44480046a23e6bcadf2218ab60fc4c
arif98741 <arifsofg@gmail.com>
2018-01-30 20:52:12 +0600

    Initials Files Added For CRUD

---
+++ 30-11-2017_crud_mysqli/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP CRUD by MYSQLi</title>
+	<style type="text/css">
+		
+		.main{
+			width: 75%;
+			margin: 10px auto;
+			background: #c0d6d8;
+		}
+		.header,.footer{
+			background: #46592dc4;
+			color: #e0e0d6;
+			text-align: center;
+			height: 87px;
+		}
+		.content{
+			min-height: 390px;
+			padding: 5px;
+		}
+		.content p{font-size:19px;}
+		.header h1, .header p, .footer h1, .footer p{margin:0px;}
+		.footer a{text-decoration:none; color:brown;}
+	</style>
+</head>
+<body>
+	<div class="main">
+		<div class="header">
+			<h1>PHP CRUD by MYSQLi</h1>
+			<p>Learn and Enjoy</p>
+		</div>
---
+++ 30-11-2017_crud_mysqli/index.php
@@ -0,0 +1,5 @@
+<?php include "header.php"; ?>
+		<div class="content">
+			
+		</div>
+<?php include "footer.php"; ?>
