718e7491b9f7f643715ab29d6e9c798eeb6e38c9
Ariful Islam <arifsofg@gmail.com>
2018-03-17 19:52:22 +0600

    Changed

--- exercise_task_wise/15-03-2018-codeigniter-crud/application/config/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-
-$route['default_controller'] = 'home';
-$route['home'] = '';
-$route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
