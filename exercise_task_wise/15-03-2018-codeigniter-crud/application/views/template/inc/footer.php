10364bef843f473ecea69bcd52cf076283aade6a
Ariful Islam <arifsofg@gmail.com>
2018-03-17 19:54:40 +0600

    CI Codes Added

---
+++ exercise_task_wise/15-03-2018-codeigniter-crud/application/views/template/inc/footer.php
@@ -0,0 +1,11 @@
+</div>
+
+</div>
+
+
+
+<script src="<?php echo site_url('assets/js/jquery.min.js'); ?>"></script>
+<script src="<?php echo site_url('assets/js/bootstrap.min.js'); ?>"></script>
+<script src="<?php echo site_url('assets/js/main.js'); ?>"></script>
+</body>
+</html>
---
+++ exercise_task_wise/15-03-2018-codeigniter-crud/assets/js/main.js
@@ -0,0 +1,15 @@
+/*!
+ * Copyright: Ariful Islam
+ * Start Date: 16-03-2018
+ * Licensed under MIT (https://github.com/arif98741/exercise)
+ * Email: arif98741@gmail.com
+ * Web: www.phpdark.com
+ 
+ */
+ 
+ $(document).ready(function(){
+	alert('hi'); 
+
+ });
+ 
+ 
