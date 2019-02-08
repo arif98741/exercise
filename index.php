40595a28984aff4468f7e48c6ce6aea5ca01ca35
Ariful <arifsofg@gmail.com>
2019-02-09 02:19:33 +0600

    Changes

--- index.php
+++ index.php
@@ -1,19 +1,14 @@
-
 <?php
+$path = pathinfo(__FILE__);
+echo '<pre>';
+print_r($path);
 
-$x = "Ariful Islam";
-if ($x > 5) {
-    if (filter_var(FILTER_VALIDATE_EMAIL)) {
-        if (FILTER_VALIDATE_FLOAT) {
-            echo "nothing";
-        } else {
-            echo "invalid float number";
-        }
-    } else {
-        echo "Invalid Email";
-    }
-} else {
-    echo "not done";
-    //this is even not done yet for the first time ever
-}
+
+
+$files = scandir(__DIR__);
+echo '<pre>';
+print_r($files);
+
+echo realpath(''); die;
+
 ?>
